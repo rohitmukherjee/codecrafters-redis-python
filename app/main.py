137f687e1c6bf8393ca117dc12fe6c07ee54da42
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2025-10-10 12:34:57 +0800

    Server now accepts and closes conections in a loop, rather than maintaining a persistent one

--- app/main.py
+++ app/main.py
@@ -6,18 +6,17 @@ MAX_BYTES: int = 1024
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    socket_connection, address = server_socket.accept()  # wait for client
-    with socket_connection:
-        print("Connected to the client")
-        while True:
-            data: bytes = socket_connection.recv(MAX_BYTES)
-            message: str = data.decode("utf-8")
-            if message is None or len(message) == 0:
-                return
-            socket_connection.send(PONG_RESPONSE_BYTES)
+    while True:
+        # We are creating a new connection for each client
+        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+        socket_connection, address = server_socket.accept()  # wait for client
+        print("Connected to the client: " + str(address))
+        data: bytes = socket_connection.recv(MAX_BYTES)
+        message: str = data.decode("utf-8")
+        if message is None or len(message) == 0:
+            return
+        socket_connection.send(PONG_RESPONSE_BYTES)
+        server_socket.close()
 
 
 if __name__ == "__main__":
