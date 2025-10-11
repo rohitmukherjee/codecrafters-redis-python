abdbdc6efb788c4ad0c360152121a5ac3e3d4f06
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2025-10-11 17:07:42 +0800

    Started using selectors as a way of multiplexing socket I/O On mac, the Selector implementation uses kqueue, whereas on Linux it uses epoll

--- app/main.py
+++ app/main.py
@@ -1,22 +1,48 @@
+import selectors
 import socket  # noqa: F401
 
 PONG_RESPONSE: str = "+PONG\r\n"
 PONG_RESPONSE_BYTES: bytes = PONG_RESPONSE.encode("utf-8")
 MAX_BYTES: int = 1024
+REDIS_PORT: int = 6379
+
+selector = selectors.DefaultSelector()
+
+
+def accept_connection(server_sock: socket):
+    client_socket, client_address = server_sock.accept()
+    print("Connected:", client_address)
+    client_socket.setblocking(False)
+    # We are registering on the client-socket here and invoking the read_message callback
+    # when the socket is ready to send
+    selector.register(client_socket, selectors.EVENT_READ, read_message)
+
+
+def read_message(client_socket: socket):
+    try:
+        data = client_socket.recv(MAX_BYTES)
+        if not data:  # client closed
+            print("Client disconnected")
+            selector.unregister(client_socket)
+            client_socket.close()
+        else:
+            client_socket.send(PONG_RESPONSE_BYTES)
+    except ConnectionError:
+        selector.unregister(client_socket)
+        client_socket.close()
 
 
 def main():
+    server_socket = socket.create_server(("localhost", REDIS_PORT), reuse_port=False)
+    server_socket.listen()
+    server_socket.setblocking(False)
+    selector.register(server_socket, selectors.EVENT_READ, accept_connection)
+
     while True:
-        # We are creating a new connection for each client
-        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-        socket_connection, address = server_socket.accept()  # wait for client
-        print("Connected to the client: " + str(address))
-        data: bytes = socket_connection.recv(MAX_BYTES)
-        message: str = data.decode("utf-8")
-        if message is None or len(message) == 0:
-            return
-        socket_connection.send(PONG_RESPONSE_BYTES)
-        server_socket.close()
+        for key, _ in selector.select():
+            callback = key.data
+            callback(key.fileobj)
+    # server_socket.close()
 
 
 if __name__ == "__main__":
